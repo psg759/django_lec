2d11737cac024bab5f454aaa66c2d36ca99d3efa
sujji <psg759@gmail.com>
2023-11-29 00:24:30 +0900

    3주차 django 개발

--- my_project/urls.py
+++ my_project/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls')),
     path("", include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
